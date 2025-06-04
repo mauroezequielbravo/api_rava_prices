60cc244d221d0f0ef579401946e4c9ca6c23b4a5
Mauro Ezequiel Bravo <mauroezequielbravo@hotmail.com>
2025-06-03 23:57:24 -0300

    add bonos

--- services/rava_scraper.py
+++ services/rava_scraper.py
@@ -62,3 +62,65 @@ def get_cedear() -> list:
 
     # print(df.to_dict(orient='records'))  # Convertir a lista de diccionarios
     return df.to_dict(orient='records')  # Convertir a lista de diccionarios
+
+
+def get_bonos() -> list:
+    # Configurar opciones para Selenium
+    options = Options()
+    options.add_argument('--headless')  # Ejecutar en modo headless
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+
+    # Inicializar el navegador
+    driver = webdriver.Chrome(options=options)
+
+    # Navegar a la página de Rava
+    url = 'https://www.rava.com/cotizaciones/bonos'
+    driver.get(url)
+
+    # Esperar a que la tabla se cargue
+    driver.implicitly_wait(10)  # Esperar hasta 10 segundos
+
+    # Obtener el contenido de la página
+    html = driver.page_source
+
+    # Cerrar el navegador
+    driver.quit()
+
+    # Parsear el HTML con BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Encontrar la tabla por su ID
+    tabla_div = soup.find('div', id='tabla')
+
+    # Extraer las filas de la tabla
+    filas = tabla_div.find_all('tr')
+
+    # Procesar las filas y extraer los datos
+    datos = []
+    for fila in filas:
+        columnas = fila.find_all(['th', 'td'])
+        datos.append([columna.get_text(strip=True) for columna in columnas])
+
+    # Crear un DataFrame de pandas
+    df = pd.DataFrame(datos, columns=[
+        'especie', 'ultimo_precio', 'porcentaje_diario', 'porcentaje_mes', 'porcentaje_anio', 
+        'precio_anterior', 'precio_apertura', 'precio_minimo', 'precio_maximo', 'hora', 'vol_nominal', 
+        'vol_efectivo'
+    ])  # Saltar la primera fila si es el encabezado
+
+    df = df.iloc[1:].reset_index(drop=True)  # Eliminar la primera fila si es el encabezado
+
+    df = df.applymap(lambda x: x.replace('.', '').replace(',', '.') if isinstance(x, str) else x)  # Reemplazar puntos y comas
+    df = df.applymap(lambda x: float(x) if isinstance(x, str) and x.replace('.', '', 1).isdigit() else x)  # Convertir a float
+
+    # remplaza los guiones por none
+    df = df.replace('-', None)
+
+    # # Mostrar las primeras filas del DataFrame
+    # print(df.head())
+
+    # print(df.to_dict(orient='records'))  # Convertir a lista de diccionarios
+    return df.to_dict(orient='records')  # Convertir a lista de diccionarios
+
+
